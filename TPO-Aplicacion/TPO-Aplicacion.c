2249dad97fb80e2a60e1f03c5c7883c0c4bfac88
tiwanacote <bertotto.maximiliano@gmail.com>
2017-09-10 23:52:20 -0300

    Se agrega poder ir de posición mayor a posición menor en la función mover_motor

--- TPO-Aplicacion/TPO-Aplicacion.c
+++ TPO-Aplicacion/TPO-Aplicacion.c
@@ -5,17 +5,14 @@
 */
 #include "Infotronic.h"
 
-
 int main(void)
 {
 	Inicializar ( );
 	
 	while( 1 )
 	{
-
 		mover_motor( 6 , 54 , 1000, 10 , 1 );
 	}
 
 	return 0 ;
-
 }
--- TPO-Primitivas/cinematica.c
+++ TPO-Primitivas/cinematica.c
@@ -9,6 +9,13 @@ extern uint8_t flag_dead_time;	// Avisa cuando aparece el tiempo muerto utilizab
 void mover_motor(uint8_t motor , uint8_t pos_ini , uint8_t pos_final , uint8_t velocidad , uint8_t aceleracion )
 {
 
+
+	// LO DE TEMP=POS_INI DEBE DE SER RESUELTO POR FUERA CON UNA VARIABLE GLOBAL DESDE LA FUNCION
+	// "MOVER_MOTORES", A LA CUAL SE LE ENTREGARÍA UN VECTOR DE POSICIONES Y EJECUTARÍA "MOVER_MOTOR"
+	// PARA TODOS LOS MOTORES!!!!!!!!!!!!!!!!!!!!!!
+	// TAMBIEN PUEDE SER QUE ESTA FUNCION DEVUELVA UN "1" SI SE ESTA MOVIENDO Y UN "0" SI TERMINO
+
+
 	static uint8_t temp ;//pos_ini;
 
 	static uint8_t primera_vez = 0;
@@ -25,11 +32,28 @@ void mover_motor(uint8_t motor , uint8_t pos_ini , uint8_t pos_final , uint8_t v
 	// Solamente incrementar posición
 	if (flag_dead_time == 1)
 	{
-		if (temp < pos_final)
-			temp = pos_ini + velocidad * cont_periodo;
+		// Si la posción inicial es menor a la final
+		if(pos_final-pos_ini < 0)
+		{
+			// Si no se llega a la posición final
+			if (temp < pos_final)
+				temp = pos_ini + velocidad * cont_periodo;
+			// Si se llega
+			else
+				temp = pos_final;
+		}
+		// Si la posición inicial es mayor a la final
 		else
-			temp = pos_final;
+		{
 
+			// Si no se llega a la posición final
+			if (temp > pos_final)
+				temp = pos_ini - velocidad * cont_periodo;
+				// Si se llega
+			else
+				temp = pos_final;
+		}
+		// Se carga la posicion transitoria en el vector de posiciones
 		pos_motor[motor - 1] = temp;
 	}
 }
