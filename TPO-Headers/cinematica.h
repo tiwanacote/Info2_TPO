7ab07251ba6f76a051489ea38775ac62e62ce76b
tiwanacote <bertotto.maximiliano@gmail.com>
2017-09-13 22:17:09 -0300

    Convierto la función mover_motores en uint8_t

--- TPO-Headers/cinematica.h
+++ TPO-Headers/cinematica.h
@@ -2,4 +2,4 @@
 #include <RegsLPC1769.h>
 
 bool mover_motor(uint8_t motor , uint8_t pos_ini , uint8_t pos_final , uint8_t velocidad , uint8_t aceleracion );
-//void mover_motores (uint8_t pos_iniciales[], uint8_t pos_finales[] , uint8_t velocidades[], uint8_t aceleraciones );
+//uint8_t mover_motores (uint8_t pos_iniciales[], uint8_t pos_finales[] , uint8_t velocidades[], uint8_t aceleraciones );
